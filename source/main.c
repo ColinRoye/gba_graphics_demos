de113e6cb0b7c66ce846c717e2b2c48e1ab32d8c
Colin Roye <colinroye@Colins-MacBook-Air.local>
2021-03-01 21:00:36 -0500

    basic demos

--- source/main.c
+++ source/main.c
@@ -12,16 +12,19 @@
 void plot(int x, int y, int color);
 void plotVerticalLine(int x, int color);
 void plotDiagnalLine(int topX, int topY, int bottomX, int bottomY, int count);
-
+void plotCube(int x, int y);
 int main()
 {
     REG_DISPCNT= DCNT_MODE3 | DCNT_BG2;
     u16* vram = VRAM;
     // vram[SCREEN_WIDTH*(SCREEN_HEIGHT/2) + SCREEN_WIDTH/2] = CLR_BLUE;
     int count = 0;
-    plot(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, CLR_RED);
-    // plotVerticalLine(SCREEN_WIDTH/2, CLR_BLUE);
-    plotDiagnalLine(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,8);
+    plotVerticalLine(SCREEN_WIDTH/2, CLR_BLUE);
+    plotDiagnalLine(0,0,SCREEN_WIDTH,SCREEN_HEIGHT, 8);
+    plotCube(SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+    plotCube(100, 100);
+
+    plot(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, CLR_BLUE);
 
     while(1);
 
@@ -52,7 +55,26 @@ void plotDiagnalLine(topX, topY, bottomX, bottomY, count){
     
     plotDiagnalLine(midpointX, mindpointY,bottomX, bottomY, count-1);
     plotDiagnalLine(topX, topY, midpointX, mindpointY, count-1);
-    // plotDiagnalLine(bottomX, bottomY, midpointX, mindpointY, count-1);
-    // plotDiagnalLine(midpointX, mindpointY,topX, topY, count-1);
+    plotDiagnalLine(bottomX, bottomY, midpointX, mindpointY, count-1);
+    plotDiagnalLine(midpointX, mindpointY,topX, topY, count-1);
+
+}
+
+void plotCube(int x, int y){
+    int offset = 10;
+    int leftBound = x-offset;
+    int rightBound =  x+offset;
+
+    int upperBound = y+offset;
+    int lowerBound = y-offset;
+
+    
+    for(int i = leftBound; i < rightBound; i++){
+        for(int j = upperBound; j > lowerBound; j--){
+            plot(i,j,CLR_RED);
+        }
+    }
+
+
 
 }
